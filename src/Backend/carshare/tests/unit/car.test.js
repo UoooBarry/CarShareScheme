1e1639631e81b4f57737e4ccb3fe201e1cf31c7d
UoooBarry <s3719368@student.rmit.edu.au>
2020-07-24 14:08:47 +0800

    added db for mock-testing

--- src/Backend/carshare/tests/unit/car.test.js
+++ src/Backend/carshare/tests/unit/car.test.js
@@ -1,8 +1,6 @@
 const Car = require('../../models/car');
 const Location = require('../../models/location');
 const chai = require('chai');
-var SequelizeMock = require('sequelize-mock');
-var DBConnectionMock = new SequelizeMock();
 //Assertion style
 const assert = chai.assert;
 
