49d4795298b5f6a3a555f94014368c22440b5fc0
UoooBarry <s3719368@student.rmit.edu.au>
2020-07-30 17:15:52 +0800

    added register and login handler

--- src/Backend/authServer/app.js
+++ src/Backend/authServer/app.js
@@ -1,6 +1,7 @@
 /* 
     Author: Yongqian Huang, created at: 23/07/2020
     updated: Yongqian Huang, 23/07/2020, Npm Init
+             Yongqian Huang, 30/07/2020, register and login handler
 */
 
 
@@ -13,6 +14,7 @@ const Customer = require('./models/customer');
 const { check, validationResult } = require('express-validator');
 const cors = require('cors')
 const passwordHash = require('password-hash');
+const login = require('./models/login');
 
 app.use(cors());
 
@@ -25,8 +27,88 @@ db.authenticate()
     .then(() => console.log('Database connected...'))
     .catch( err => console.log(`DB err: ${err}`))
 
+app.post('/api/register',
+    [
+        check('email').isEmail().withMessage("Email format unmatched"),
+        check('password').isLength({min: 6}).withMessage('Password should at least 6 digits long'),
+        check('first_name').not().isEmpty().withMessage('FirstName cannot be empty'),
+        check('family_name').not().isEmpty().withMessage('FamilyName cannot be empty'),
+        check('contact_number').not().isEmpty().withMessage('Contact cannot be empty')
+    ]
+    , async (req,res) => {
+        
+        const errs = validationResult(req);
+         //check and return errors
+        if(!errs.isEmpty()){
+            let errors = [];
+            errs.array().forEach(
+                err => errors.push(err.msg)
+            )
+            res.json({
+                message: "fail",
+                errors});
+            
+            return;
+        }
 
-    
+        //Create a new user if it is correct
+        var customer = await Customer.create({
+            first_name: req.body.first_name,
+            family_name: req.body.family_name,
+            gender: req.body.gender,
+            date_of_birth: req.body.date_of_birth,
+            contact_number: req.body.contact_number
+        });
+        
+        //Create login for user
+        await login.create({
+            Email: req.body.email,
+            password: passwordHash.generate(req.body.password),
+            user_id: customer.id
+        });
+
+        res.json({
+            message: 'success'
+        });
+});
+
+app.post('/api/login',(req,res) => {
+    const email = req.body.email;
+    if(!email) res.sendStatus(403);
+
+    login.findOne({
+        where: {
+            Email: email
+        },
+        include: [
+            {model: Customer}
+        ]
+    })
+        .then(async (login) => {
+            const customer = await login.getCustomer();
+            if(login != null && passwordHash.verify(req.body.password, login.password)){    
+                JWT.sign({customer}, process.env.ACCESS_TOKEN_SECRET, (err, token) => {
+                    if(err){
+                        console.log(err);
+                      }
+                      res.json({
+                        token,
+                        message: 'success'
+                      });
+                })
+            }else{
+                res.json({message: 'Information unmatched'});
+            }
+        })
+});
+
+app.post('/api/validate', (req, res) => {
+    const token = req.body.token;
+    JWT.verify(token,process.env.ACCESS_TOKEN_SECRET, (err, data)=> {
+        res.json(data);
+    })
+})
+
 // error handler
 app.use(function(err, req, res, next) {
     // set locals, only providing error in development
--- src/Backend/authServer/models/login.js
+++ src/Backend/authServer/models/login.js
@@ -1,11 +1,17 @@
+/* 
+    Author: Yongqian Huang, created at: 23/07/2020
+    updated: Yongqian Huang, 23/07/2020, Creation of login model
+*/
+
 const Sequelize = require('sequelize');
 const db = require('../config/database');
-
+const Customer = require('./customer');
 
 //scheme
 const login = db.define('logins',{
     Email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        primaryKey: true
     },
     password: {
         type: Sequelize.STRING
@@ -14,5 +20,7 @@ const login = db.define('logins',{
         type: Sequelize.INTEGER
     }
 });
+
+login.belongsTo(Customer,{foreignKey: 'user_id'});
 
 module.exports = login;
