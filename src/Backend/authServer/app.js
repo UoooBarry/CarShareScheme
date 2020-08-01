631f83ce9035da0b49c5f4d0d14d678dcd6d9c9f
UoooBarry <s3719368@student.rmit.edu.au>
2020-08-01 21:36:16 +0800

    added api testings with Ci

--- src/Backend/authServer/app.js
+++ src/Backend/authServer/app.js
@@ -8,13 +8,10 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const JWT = require('jsonwebtoken');
 const db = require('./config/database');
-const Customer = require('./models/customer');
-const { check, validationResult } = require('express-validator');
+const indexRouter = require('./routes/index.js');
 const cors = require('cors')
-const passwordHash = require('password-hash');
-const login = require('./models/login');
+
 
 app.use(cors());
 
@@ -25,88 +22,9 @@ app.use(express.urlencoded({ extended: false }));
 //catch database connection
 db.authenticate()
     .then(() => console.log('Database connected...'))
-    .catch( err => console.log(`DB err: ${err}`))
+    .catch( err => console.log(`DB connected fail: ${err}`))
 
-app.post('/api/register',
-    [
-        check('email').isEmail().withMessage("Email format unmatched"),
-        check('password').isLength({min: 6}).withMessage('Password should at least 6 digits long'),
-        check('first_name').not().isEmpty().withMessage('FirstName cannot be empty'),
-        check('family_name').not().isEmpty().withMessage('FamilyName cannot be empty'),
-        check('contact_number').isLength({min:10, max:10}).withMessage('Contact number should be 10 digits').matches(/^[0][0-9]*$/).withMessage('Contact must be numbers start with 0')
-    ]
-    , async (req,res) => {
-        const errs = validationResult(req);
-         //check and return errors
-        if(!errs.isEmpty()){
-            let errors = [];
-            errs.array().forEach(
-                err => errors.push(err.msg)
-            )
-            res.json({
-                message: "fail",
-                errors});
-            
-            return;
-        }
-
-        //Create a new user if it is correct
-        var customer = await Customer.create({
-            first_name: req.body.first_name,
-            family_name: req.body.family_name,
-            gender: req.body.gender,
-            date_of_birth: req.body.date_of_birth,
-            contact_number: req.body.contact_number
-        });
-        
-        //Create login for user
-        await login.create({
-            Email: req.body.email,
-            password: passwordHash.generate(req.body.password),
-            user_id: customer.id
-        });
-
-        res.json({
-            message: 'success'
-        });
-});
-
-app.post('/api/login',(req,res) => {
-    const email = req.body.email;
-    if(!email) res.sendStatus(403);
-
-    login.findOne({
-        where: {
-            Email: email
-        },
-        include: [
-            {model: Customer}
-        ]
-    })
-        .then(async (login) => {
-            const customer = await login.getCustomer();
-            if(login != null && passwordHash.verify(req.body.password, login.password)){    
-                JWT.sign({customer}, process.env.ACCESS_TOKEN_SECRET, (err, token) => {
-                    if(err){
-                        console.log(err);
-                      }
-                      res.json({
-                        token,
-                        message: 'success'
-                      });
-                })
-            }else{
-                res.json({message: 'Information unmatched'});
-            }
-        })
-});
-
-app.post('/api/validate', (req, res) => {
-    const token = req.body.token;
-    JWT.verify(token,process.env.ACCESS_TOKEN_SECRET, (err, data)=> {
-        res.json(data);
-    })
-})
+app.use('/api', indexRouter);
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -119,4 +37,6 @@ app.use(function(err, req, res, next) {
     res.json(err);
   });
 
-app.listen(4000, ()=> console.log("Auth sever is running on port 4000"))
+app.listen(4000, ()=> console.log("Car share scheme system authentication sever is running on port 4000 ..."));
+
+module.exports = app;
---
+++ src/Backend/authServer/tests/integration/login.test.js
@@ -0,0 +1,74 @@
+/* 
+    Author: Yongqian Huang, created at: 01/08/2020
+    updated: Yongqian Huang, 24/07/2020, Init creation for api testing
+*/
+
+
+const chai = require('chai');
+//Assertion style
+const should = chai.should();
+const app = require('../../app');
+const db = require('../../config/database');
+const Customer =require('../../models/customer');
+const Login =require('../../models/login');
+
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+
+describe('POST /register', () => {
+    before((done) => {
+        db.authenticate()
+        .then(() => done())
+        .catch( err => done(err));
+        Customer.sync()
+                .then(Login.sync());
+
+    })
+
+    after((done) => {
+        db.close()
+            .then(() => done())
+            .catch(err => done(err));
+    })
+
+    beforeEach((done) => {
+        Login.destroy({where: {}, truncate: true})
+             .then(() => {
+                Customer.destroy({
+                    where: {},
+                    truncate: true
+                });
+                done();
+             })
+        
+    })
+
+    it('Register an account with valid information', (done) => {
+        chai.request(app).post('/api/register')
+                    .send({
+                        email: "dummy@gmail.com",
+                        password: "123456",
+                        first_name: "Dummy",
+                        family_name: "Dummy",
+                        contact_number: "0433838111"
+                    })
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.have.property('message').eql('success');
+                        done();
+                    })
+    });
+
+    it('Register an account with invalid information', (done) => {
+        chai.request(app).post('/api/register')
+                    .send({
+                        email: "Not an email"
+                    })
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.have.property('message').eql('fail');
+                        done();
+                    })
+    })
+})
