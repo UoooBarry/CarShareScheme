b98c981571db58b0e421a161a33bc8a557ad3ec8
UoooBarry <s3719368@student.rmit.edu.au>
2020-08-02 14:43:56 +0800

    added infra env script

---
+++ infra/prod.env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set +ex
+
+echo "Genrating production environment config..."
+echo "PROD_DBHOST = $PROD_DBHOST"
+echo "PROD_DBPORT = $PROD_DBPORT"
+echo "PROD_DBNAME = $PROD_DBNAME"
+echo "PROD_DBUSER = $PROD_DBUSER"
+echo "PROD_DBPASS = $PROD_DBPASS"
