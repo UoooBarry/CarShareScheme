45b4d0e64b079cf937df4386e712b6a462026707
UoooBarry <s3719368@student.rmit.edu.au>
2020-08-06 17:57:33 +0800

    removed create customer function in car share

--- src/Backend/carshare/repository/customerRepository.js
+++ src/Backend/carshare/repository/customerRepository.js
@@ -19,17 +19,6 @@ class customerRepository{
         return await Customer.findAll({where: {id: id}});
     }
 
-    async create(first_name, family_name, gender, date_of_birth, contact_number){
-        const customer = await Customer.create({
-            first_name: first_name,
-            family_name: family_name,
-            gender: gender,
-            date_of_birth: date_of_birth,
-            contact_number: contact_number
-        });
-
-        return customer;
-    }
 }
 
 module.exports = new customerRepository();
