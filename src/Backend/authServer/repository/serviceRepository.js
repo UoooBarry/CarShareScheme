c631d40e1656fba7434a7f0140a7a104f702b2b0
UoooBarry <s3719368@student.rmit.edu.au>
2020-08-07 17:33:07 +0800

    added mobile validation

---
+++ src/Backend/authServer/repository/serviceRepository.js
@@ -0,0 +1,59 @@
+/**********************************************************************
+ * @AUTHOR YONGQIAN HUANG, CREATED AT 07/08/2020 MANAGE MICROSERVICES *
+ **********************************************************************/
+require('dotenv').config();
+const axios = require('axios').default;
+const cache = require('memory-cache');
+const Nexmo = require('nexmo');
+
+ class serviceRepository {
+    constructor(){
+        this.expired = 60 * 1000;
+        this.nexmo = new Nexmo({
+            apiKey: process.env.NEXMO_KEY,
+            apiSecret: process.env.NEXMO_SECRET,
+            applicationId: process.env.NEXMO_ID
+        });
+    }
+
+    
+
+    async getRecaptchaRes(user_response){
+        const params = new URLSearchParams();
+        params.append('secret', '6LcTY7sZAAAAAGZNzQgsa1Q9uZWpP8EThE5-tYaQ');
+        params.append('response', user_response);
+
+        const response = await axios.post("https://www.google.com/recaptcha/api/siteverify", params, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } });
+        return response;
+    }
+
+    getOneCode(){
+        //generate a number between 1000 - 9999: 4 digits
+        return Math.floor(Math.random() * 8999) + 1000;
+    }
+
+    async putOneCode(phone){
+        const code = await this.getOneCode();
+        await cache.put(phone, code, this.expired);
+        const from = 'Car share';
+        const to = `61${phone}`;
+        const text = `Your one time register code is ${code}`;
+
+        this.nexmo.message.sendSms(from, to, text, (err,res) => {
+            if(err)
+                console.log(err);
+        });
+
+    }
+
+    verifyOneCode(phone, code){
+        const expected = cache.get(phone);
+        if(code == expected){
+            return true;
+        }else{
+            return false;
+        }
+    }
+ }
+
+ module.exports = new serviceRepository();
--- src/Frontend/carshare/src/components/RegisterForm.vue
+++ src/Frontend/carshare/src/components/RegisterForm.vue
@@ -1,40 +1,46 @@
 <template>
     <div class="parallax" style="margin-bottom: 60px;">
-        <div class="row">
-            <div class="container">
-                <form @submit.prevent="register">
-                    <div class="form-group">
-                        <label for="email">Email: </label>
-                        <input class="form-control" v-model="email" id="email" name="email" type="text">
-                    </div>
+        <div class="container">
+            <form @submit.prevent="register">
+                <div class="form-group">
+                    <label for="email">Email: </label>
+                    <input class="form-control" v-model="email" id="email" name="email" type="text">
+                </div>
 
-                    <div class="form-group">
-                        <label for="password">Password: </label>
-                        <input class="form-control" v-model="password" id="password" name="password" type="password">
-                    </div>
+                <div class="form-group">
+                    <label for="password">Password: </label>
+                    <input class="form-control" v-model="password" id="password" name="password" type="password">
+                </div>
 
-                    <div class="form-group">
-                        <label for="retype">Retype Password: </label>
-                        <input class="form-control" v-model="retype" id="retype" name="retype" type="password">
-                    </div>
-                    
-                    <div class="form-group">
-                        <label for="first_name">First Name: </label>
-                        <input class="form-control" v-model="first_name" id="first_name" name="first_name" type="text">
-                    </div>
+                <div class="form-group">
+                    <label for="retype">Retype Password: </label>
+                    <input class="form-control" v-model="retype" id="retype" name="retype" type="password">
+                </div>
+                
+                <div class="form-group">
+                    <label for="first_name">First Name: </label>
+                    <input class="form-control" v-model="first_name" id="first_name" name="first_name" type="text">
+                </div>
 
-                    <div class="form-group">
-                        <label for="family_name">Family Name: </label>
-                        <input class="form-control" v-model="family_name" id="family_name" name="family_name" type="text">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="contact_number">Contact number: </label>
-                        <input class="form-control" v-model="contact" id="contact" name="contact" type="number">
-                    </div>
-                    <input class="btn btn-primary" type="submit" value="Register">
-                </form>
-            </div>
+                <div class="form-group">
+                    <label for="family_name">Family Name: </label>
+                    <input class="form-control" v-model="family_name" id="family_name" name="family_name" type="text">
+                </div>
+                
+                
+                <div class="form-group mb-3">
+                    <label for="contact_number">Contact number: </label>
+                    <input class="form-control" v-model="contact" id="contact" name="contact" type="number" placeholder="Put an australian conatct number start with 4" >
+                    <button type="button" class="btn btn-primary float-right" id="get_code" v-on:click="get_code" > Get Validation Code </button>
+                </div>
+                
+                <br>
+                <div class="form-group">
+                    <label for="validation_code">Validation code: </label>
+                    <input class="form-control" v-model="validation_code" id="validation_code" name="validation_code" type="number" required>
+                </div>
+                <input class="btn btn-primary" type="submit" value="Register">
+            </form>
         </div>
     </div>
 </template>
@@ -51,18 +57,17 @@ export default {
                 this.flashMessage.error({title: 'Register detail', message: 'Password and retype password not matched.'});
                 return;
             }
-
             this.$axios.post(`${this.$auth}/register`,{
                 email: this.email,
                 password: this.password,
                 first_name: this.first_name,
                 family_name: this.family_name,
                 contact_number: this.contact,
-                recaptcha_token: token
+                recaptcha_token: token,
+                code: this.validation_code
             })
                 .then( (res) => {
                     if(res.data.message == "fail"){
-                        this.flashMessage.setStrategy('multiple');
                         res.data.errors.forEach(error => {
                             this.flashMessage.error({title: 'Register detail', message: error});
                         });
@@ -74,6 +79,36 @@ export default {
                     
                 })
                 .catch(err => console.log(err));
+        },
+        get_code(){
+            this.btn_countdown();
+            const number = document.getElementById('contact').value;
+            this.$axios.get(`${this.$auth}/getCode?contact_number=${number}`)
+            .then((res) => {
+                if(res.data.message === "success"){
+                    this.flashMessage.success({title: 'Send code success!', message: res.data.description});
+                }else{
+                    res.data.errors.forEach(error => {
+                        this.flashMessage.error({title: 'Register detail', message: error});
+                    });
+                }
+            })
+            .catch(err => console.log(err));
+
+        },
+        btn_countdown(){
+            const btn = document.getElementById('get_code');
+            btn.disabled = true;
+            var time = 60;
+            setInterval(() => {
+                time --;
+                btn.innerHTML = `${time}s Reamin`;
+                if(time <= 0){
+                    btn.disabled = false;
+                    btn.innerHTML = "Get Validation Code";
+                }
+            }, 1000);
+            
         }
     },
     data() {
@@ -83,8 +118,12 @@ export default {
             first_name: '',
             family_name: '',
             password: '',
-            retype: ''
+            retype: '',
+            validation_code: ''
         }
+    },
+    created(){
+        this.flashMessage.setStrategy('multiple');
     }
 }
 </script>
