b7745403a8d80f857f9e864e74cc9ca2ddcf0c96
UoooBarry <s3719368@student.rmit.edu.au>
2020-08-08 23:42:08 +0800

    cat inventory.yml

--- ansible/run_ansible.sh
+++ ansible/run_ansible.sh
@@ -4,6 +4,6 @@ set +ex
 #generate inventory
 echo "Generating inventory file"
 sh inventory.sh > inventory.yml
-
+cat inventory.yml
 
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.yml -e 'record_host_keys=True' -u ec2-user playbook.yml
