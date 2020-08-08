25f62d04588f2c48d14a44f2e15fa56b2ba7e172
UoooBarry <s3719368@student.rmit.edu.au>
2020-08-08 23:52:04 +0800

    use inventory.yml static

--- ansible/run_ansible.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set +ex
-
-#generate inventory
-echo "Generating inventory file"
-sh inventory.sh > inventory.yml
-cat inventory.yml
-
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.yml -e 'record_host_keys=True' -u ec2-user playbook.yml
