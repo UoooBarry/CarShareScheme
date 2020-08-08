6a544bb86fa393982f2ababfef3324bc6c4a30a9
UoooBarry <s3719368@student.rmit.edu.au>
2020-08-08 23:25:47 +0800

    added playbook .yml

--- ansible/run_ansible.sh
+++ ansible/run_ansible.sh
@@ -6,4 +6,4 @@ echo "Generating inventory file"
 sh inventory.sh > inventory.yml
 
 
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.yml -e 'record_host_keys=True' -u ec2-user
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.yml -e 'record_host_keys=True' -u ec2-user playbook.yml
