28f57a3917b8f54a43fb16ff59b2b0a59b21cf6b
noobfromvn99 <s3673712@student.rmit.edu.au>
2020-08-11 16:28:32 +1000

    add Profile page and import FontAwesome libary

--- src/Frontend/carshare/src/views/Profile.vue
+++ src/Frontend/carshare/src/views/Profile.vue
@@ -1,14 +1,31 @@
 <template>
-    <UserDescription />
+  <div class="profile" style>
+    <h1>This is a Profile page</h1>
+    <AvatarHolder />
+
+    <InformationProfile />
+    </div>
 </template>
+<style>
 
-<script>
-import UserDescription from '@/components/UserDescription';
-
-export default {
-    name: "Profile",
-    components:{
-        UserDescription
-    }
+.profile {
+  height: 1000px;
+  background-image: url("../assets/img/bg.jpg");
+  background-repeat: none;
+  background-size: cover;
+  background-attachment: fixed;
 }
+
+</style>
+  
+<script>
+import AvatarHolder from "@/components/AvatarHolder";
+import InformationProfile from "@/components/InformationProfile";
+export default {
+  name: "Profile",
+  components: {
+    AvatarHolder,
+    InformationProfile
+  },
+};
 </script>
