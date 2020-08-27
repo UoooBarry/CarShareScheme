1e461249950204341891d27efdb13ad70818df9e
DanaSelene <dawanamo@gmail.com>
2020-08-28 01:46:54 +1000

    admin-sub header
    added in an subheader and I have changed the main header's text into white

--- src/Frontend/carshare/src/components/layouts/AdminHeader.vue
+++ src/Frontend/carshare/src/components/layouts/AdminHeader.vue
@@ -1,19 +1,21 @@
 <template>
-  <header>
-    <nav class="navbar navbar-light navbar-expand-md navbar-transparency adminheader">
+  <header >
+    <nav  id = "mainHeader" class="navbar navbar-light navbar-expand-md navbar-transparency">
         <Brand />
-        <div class="container">
-            <div class="collapse navbar-collapse"
-                id="navcol-1">
-                <ul class="nav navbar-nav ml-auto">
-                    <li class="nav-item" role="presentation"  v-bind:class="{'nav-item active': getActive('AdminUser')}" ><a class="nav-link" href="#">User list</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="#">Car list</a></li>
-                   
-                    <li class="nav-item" role="presentation" id="login" style="display: none"><a class="nav-link" href="/">Login</a></li>
-                </ul>
-            </div>
-        </div>
     </nav>
+    <nav class="subHeader">
+                <ul >
+
+                    <!-- <a class="navbar-brand-img" href="#" >  -->
+                        <img class="brand-img" src="../../assets/home.png" alt="Home logo">
+                        <!-- </a>  -->
+                    <div id="text-link">
+                    <a class="navbar-brand active" href="#" ><li> USER LIST </li></a>
+                    <a class="navbar-brand" href="#"><li > CAR LIST </li></a>
+                    </div>
+                    
+                </ul>
+        </nav>
 </header>
 </template>
 
@@ -35,9 +37,53 @@ export default {
 </script>
 
 <style scoped>
-.adminheader{
-    background: #648CB5;
-    color: whitesmoke;
+
+#mainHeader{
+    background-color:#648CB5;
+    }
+.subHeader{
+    background-color:whitesmoke;
+    max-height:45px;
+    
+    }
+
+ul{
+    display:flex;  
+    list-style:none;
+}
+li{
+    padding: 3px 20px;
+    }
+img {
+  max-width: 30px;
+  height: 30px;
+  position:fix;
 }
 
+.navbar-brand{
+    font-family: 'Suisseintl webm', sans-serif;
+    font-weight: 500;
+    letter-spacing: 0.3px;
+    text-transform: none;
+    font-size:1em;
+    margin-right: -0px;
+    color:rgba(65, 119, 153, 0.726);
+    height:35px;
+}
+
+.active{
+  
+    background-color:#648CB5;
+  color: aliceblue;
+}
+
+ .navbar-brand:hover {
+    background-color:#648cb5e7;
+    color: aliceblue;
+ }
+
+ #text-link{
+     padding-left: 15px;
+ }
+  
 </style>
