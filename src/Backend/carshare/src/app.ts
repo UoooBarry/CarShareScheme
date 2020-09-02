3b386a6e50988a371eab0a0906eab603c4606782
UoooBarry <s3719368@student.rmit.edu.au>
2020-09-03 04:59:30 +0800

    export app for testing\

--- src/Backend/carshare/src/app.ts
+++ src/Backend/carshare/src/app.ts
@@ -70,4 +70,4 @@ app.use( (err: any, req: Request, res: Response, next: NextFunction) => {
 
 app.listen(3000, () => console.log('Server in running on port 3000...'));
 
-// module.exports = app;
+module.exports = app;
