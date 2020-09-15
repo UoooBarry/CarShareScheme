6bd082129dba344fd716a179682d6c3440b83bfd
SHUYUAN ZHANG <s3632704@student.rmit.edu.au>
2020-09-15 21:00:20 +1000

    Add information to FAQ page

--- src/Frontend/carshare/src/views/FAQ.vue
+++ src/Frontend/carshare/src/views/FAQ.vue
@@ -1,10 +1,40 @@
 <template>
   <div class="container">
-    <h1 class="text-md-left mb-5 px-2 font-weight-bold font-italic">
-        1. Question 1?
-    </h1>
-    <p class="text-md-left mb-5 px-2"> 
-        Some answers
+    <h1 class="text-md-left mb-5 px-2 font-weight-bold font-italic">1. How do I book online?</h1>
+    <p class="text-md-left mb-5 px-2">
+      1.Our home page features 'Car list' to make booking easier.
+      <br />2.Click the vehicle you wish to rent and then you will see the information and price of the vehicle.
+      <br />3. Click on the check out button and it will take you to the login page and click "LOGIN NOW". Even if you don't have a account from us,Please Click "sign up for free" and edit your information the submit.
+      <br />4.After login,you can choose the Start dates and renting periods of the vehicle, then click "Next".
+      <br />5. You will see the Pick up location and Return date, then Press "Confirm Order".
+      <br />6. After Press "Confirm Order", you will just need to select the payment option and enter you payment information to complete the order.
+      <br />
     </p>
-    </div>
-    </template>
+    <h1
+      class="text-md-left mb-5 px-2 font-weight-bold font-italic"
+    >2. How will I know my book is comfirmed?</h1>
+    <p class="text-md-left mb-5 px-2">
+      1. After the payment, the website will display the receipt include your
+      <br />" Contact information "
+      <br />" Pick up information "
+      <br />2.You will received a message by your registration number after you confirm the order.
+      <br />3. Or you can check your comfirmation in your Rent history.
+      <br />
+    </p>
+    <h1
+      class="text-md-left mb-5 px-2 font-weight-bold font-italic"
+    >3. How old do I have to be rent a vehicle?</h1>
+    <p class="text-md-left mb-5 px-2">
+      1.All renters must be aged at least 21 years old at the commencement of the rental.
+      <br />2.Any renters aged 21 to 24 inclusive (whether the main or additional driver) incur an age surcharge per day, and can access our standard range of vehicles. Ultimate Protection (UP) is not available to drivers in this age group.
+      <br /> 
+    </p>
+    <h1
+      class="text-md-left mb-5 px-2 font-weight-bold font-italic"
+    >4. What is my responsisibility when renting a vehicle in PBD Car Share?</h1>
+    <p class="text-md-left mb-5 px-2">
+      From the time you collect your vehicle, you are financially responsible for any loss of damage to the vehicle. This includes damage caused outside of your control including hailstorms. You are also bound by PBD's standard Terms & Policy as agreed to when you confirm the order.
+      <br /> 
+    </p>
+  </div>
+</template>
