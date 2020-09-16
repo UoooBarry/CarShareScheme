fda28135515ef00876b7f08a6a97d62be9c88ad5
SHUYUAN ZHANG <s3632704@student.rmit.edu.au>
2020-09-16 19:51:45 +1000

    Term page

--- src/Frontend/carshare/src/views/TermAndPolicy.vue
+++ src/Frontend/carshare/src/views/TermAndPolicy.vue
@@ -1,70 +1,117 @@
 <template>
   <div class="container">
-    <h1 class="text-md-left mb-5 px-2"> Terms & Policy</h1>
-    <p class="text-md-left mb-5 px-2">http://carshare.uooobarry.com is a limited company registered in Australia (Number: xxxxxx), whose registered address is at 123 Melbourne St, Melbourne, VIC 3000. These terms & conditions, together with our privacy notice and cookie statement, constitute our agreement with you, for the services we provide in arranging for the supply of car hire. For the avoidance of doubt, Your hire of any car is subject to the terms & conditions of car hire imposed by the car hire company and to the corresponding laws of the country and/or state in which the rental takes place. Individual restrictions may also apply to your car hire. You will be provided with a car by us either directly or as agent. The capacity in which we act depends on our relationship with the car hire company.</p>
+    <h1 class="text-md-left mb-5 px-2">Terms & Policy</h1>
+    <p
+      class="text-md-left mb-5 px-2"
+    >http://carshare.uooobarry.com is a limited company registered in Australia (Number: xxxxxx), whose registered address is at 123 Melbourne St, Melbourne, VIC 3000. These terms & conditions, together with our privacy notice and cookie statement, constitute our agreement with you, for the services we provide in arranging for the supply of car hire. For the avoidance of doubt, Your hire of any car is subject to the terms & conditions of car hire imposed by the car hire company and to the corresponding laws of the country and/or state in which the rental takes place. Individual restrictions may also apply to your car hire. You will be provided with a car by us either directly or as agent. The capacity in which we act depends on our relationship with the car hire company.</p>
 
-    <p class="text-md-left mb-5 px-2"> These terms apply to any booking that you make with us on or through this website, so you should make sure that you are aware of their contents. Both our terms and those of the car hire companies we work with contain some exclusions and limitations of liability.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >These terms apply to any booking that you make with us on or through this website, so you should make sure that you are aware of their contents. Both our terms and those of the car hire companies we work with contain some exclusions and limitations of liability.</p>
 
-    <p>Your booking on this website is conditional on you accepting our terms. If you do not agree with any part of them, you must not proceed with your booking. By confirming that you wish to book a car through this website you,, indicate that you have read and understood and accept our terms. If there is any part that you do not fully understand, or if you have a query about the car hire or any other product, please Contact us.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >Your booking on this website is conditional on you accepting our terms. If you do not agree with any part of them, you must not proceed with your booking. By confirming that you wish to book a car through this website you,, indicate that you have read and understood and accept our terms. If there is any part that you do not fully understand, or if you have a query about the car hire or any other product, please Contact us.</p>
 
-    <h1>Privacy Notice</h1>
+    <h1 class="text-md-left mb-5 px-2">Privacy Notice</h1>
 
-    <p>This Privacy Notice explains how we collect, use and protect any information about you. It also tells you how to get in touch if you have any questions, which we will be more than happy to answer. If you’re interested in what we do with cookies and similar technologies, please check out our Cookie Statement.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >This Privacy Notice explains how we collect, use and protect any information about you. It also tells you how to get in touch if you have any questions, which we will be more than happy to answer. If you’re interested in what we do with cookies and similar technologies, please check out our Cookie Statement.</p>
 
-    <p>We may change this Notice from time to time; so please check back here for any updates.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >We may change this Notice from time to time; so please check back here for any updates.</p>
 
-    <p>We offer a range of services related to online car rental, including products and services such as insurance, through our own websites, We also do this through our business partners’ websites, social media and other platforms. This Notice applies to all the personal data we collect while we’re doing this, or when you contact us by email, live chat, phone or post.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >We offer a range of services related to online car rental, including products and services such as insurance, through our own websites, We also do this through our business partners’ websites, social media and other platforms. This Notice applies to all the personal data we collect while we’re doing this, or when you contact us by email, live chat, phone or post.</p>
 
-    <h1>Minimum / Maximum Age Limitations</h1>
+    <h1 class="text-md-left mb-5 px-2">Minimum / Maximum Age Limitations</h1>
 
-    <p>In most locations, the minimum hiring age is 21 years. Additional fees can apply with certain car hire companies and in certain countries for drivers aged under 25 years or over 65 years: where the correct driver age has been submitted with the booking request, we will advise of any likely fees. Minimum age requirements on larger cars may also be higher at some locations. Please check with our Contact Centre if you have any questions or concerns about this matter.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >In most locations, the minimum hiring age is 21 years. Additional fees can apply with certain car hire companies and in certain countries for drivers aged under 25 years or over 65 years: where the correct driver age has been submitted with the booking request, we will advise of any likely fees. Minimum age requirements on larger cars may also be higher at some locations. Please check with our Contact Centre if you have any questions or concerns about this matter.</p>
 
-    <h1>Late Pick-ups / Early Drop-offs</h1>
+    <h1 class="text-md-left mb-5 px-2">Late Pick-ups / Early Drop-offs</h1>
 
-    <p>Your rental starts and finishes according to the pick-up and drop-off dates and times detailed on your voucher / eVoucher. Unfortunately, we are unable to provide refunds for unused time if a car is picked up late or dropped off early. If you are unable to pick the car up at the agreed time and date, it is essential you contact us as soon as possible. If you do not, there is no guarantee that the car will still be available – and you will not be entitled to any refund.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >Your rental starts and finishes according to the pick-up and drop-off dates and times detailed on your voucher / eVoucher. Unfortunately, we are unable to provide refunds for unused time if a car is picked up late or dropped off early. If you are unable to pick the car up at the agreed time and date, it is essential you contact us as soon as possible. If you do not, there is no guarantee that the car will still be available – and you will not be entitled to any refund.</p>
 
-    <h1>If We Make Changes to Your Booking</h1>
+    <h1 class="text-md-left mb-5 px-2">If We Make Changes to Your Booking</h1>
 
-    <p>Occasionally it may be necessary for changes to be made to your booking (after acceptance) by us or by the car hire company. In all such cases we will advise you as soon as reasonably possible before pick-up and, if the proposed changes are unacceptable, you will be offered a full refund. However, in such circumstances, we shall have no additional liability in respect of any direct or indirect losses you may suffer as a result of such changes.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >Occasionally it may be necessary for changes to be made to your booking (after acceptance) by us or by the car hire company. In all such cases we will advise you as soon as reasonably possible before pick-up and, if the proposed changes are unacceptable, you will be offered a full refund. However, in such circumstances, we shall have no additional liability in respect of any direct or indirect losses you may suffer as a result of such changes.</p>
 
-    <h1>Personal data you give us</h1>
+    <h1 class="text-md-left mb-5 px-2">Personal data you give us</h1>
 
-    <p>We store and use the information that you give us. When you make a booking, we need at least your name, email address and payment information. We might also ask for your home address, phone number and date of birth, and the names of any additional drivers.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >We store and use the information that you give us. When you make a booking, we need at least your name, email address and payment information. We might also ask for your home address, phone number and date of birth, and the names of any additional drivers.</p>
 
-    <p>If you get in touch with us (by phone, by email or through social media, for example) we will collect information from you there too.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >If you get in touch with us (by phone, by email or through social media, for example) we will collect information from you there too.</p>
 
-    <p>After your booking, we may ask you to provide a review to help us improve our services, and to help ensure future customers get exactly what they’re looking for.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >After your booking, we may ask you to provide a review to help us improve our services, and to help ensure future customers get exactly what they’re looking for.</p>
 
-    <p>There are also other ways you might give us information. For example, if you’re browsing with your mobile device, you can share your current location with us, so we can provide you with the best possible service. To make things easier, you can also open a user account, which lets you review and manage your bookings, as well as saving your personal settings and your passport and driving licence details. It also makes it easier to make future bookings.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >There are also other ways you might give us information. For example, if you’re browsing with your mobile device, you can share your current location with us, so we can provide you with the best possible service. To make things easier, you can also open a user account, which lets you review and manage your bookings, as well as saving your personal settings and your passport and driving licence details. It also makes it easier to make future bookings.</p>
 
-    <h1>Personal data you give us about others</h1>
+    <h1 class="text-md-left mb-5 px-2">Personal data you give us about others</h1>
 
-    <p>You might add someone as an additional driver, or you might make a booking on behalf of someone else, for example, a friend, family member or colleague. If you do, please make sure that that person knows you’re giving us their details, and has accepted the way we handle their personal data (as described in this Notice). This is your responsibility.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >You might add someone as an additional driver, or you might make a booking on behalf of someone else, for example, a friend, family member or colleague. If you do, please make sure that that person knows you’re giving us their details, and has accepted the way we handle their personal data (as described in this Notice). This is your responsibility.</p>
 
-    <h1>Personal data we collect automatically</h1>
+    <h1 class="text-md-left mb-5 px-2">Personal data we collect automatically</h1>
 
-    <p>When you make a booking, we record what platform you made it on, and how you got to our platform (if you came through from another site, for example).</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >When you make a booking, we record what platform you made it on, and how you got to our platform (if you came through from another site, for example).</p>
+    <h1 class="text-md-left mb-5 px-2">Confirming your booking and taking payment</h1>
 
-    <h1>Confirming your booking and taking payment</h1>
+    <p
+      class="text-md-left mb-5 px-2"
+    >In most cases, we will confirm your booking immediately using the email address provided when the booking is made. Sometimes, we need to wait for the car hire company to confirm that a car is available. We will tell you straight away if this happens, and temporarily block the money on your payment card. As soon as they confirm your car, we will let you know, and take the money from your card.</p>
 
-    <p>In most cases, we will confirm your booking immediately using the email address provided when the booking is made. Sometimes, we need to wait for the car hire company to confirm that a car is available. We will tell you straight away if this happens, and temporarily block the money on your payment card. As soon as they confirm your car, we will let you know, and take the money from your card.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >Until your car is confirmed, you can ‘void’ the payment at any time. Just hit ‘Manage booking’ at the top of our site, enter the email address provided when making the booking and reservation number, then hit ‘Cancel Booking’. We will unblock the money on your card.</p>
 
-    <p>Until your car is confirmed, you can ‘void’ the payment at any time. Just hit ‘Manage booking’ at the top of our site, enter the email address provided when making the booking and reservation number, then hit ‘Cancel Booking’. We will unblock the money on your card.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >Very rarely, a car hire company can’t confirm that a car is available. If this happens, we will tell you, and unblock the money on your card.</p>
 
-    <p>Very rarely, a car hire company can’t confirm that a car is available. If this happens, we will tell you, and unblock the money on your card.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >When you book a car or other product on our site, we will email you to confirm its availability within 48 hours of booking (or, if earlier, at least 24 hours before your pick-up time). We will then send you a booking confirmation.</p>
 
-    <p>When you book a car or other product on our site, we will email you to confirm its availability within 48 hours of booking (or, if earlier, at least 24 hours before your pick-up time). We will then send you a booking confirmation.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >You will not have a contract until we have confirmed your booking and taken the payment. We will send all correspondence to the address provided when making the booking. For the avoidance of doubt, nothing in these terms shall entitle any third party to any benefit or rights pursuant to the Contracts (Rights of Third Parties) Act 1999.</p>
 
-    <p>You will not have a contract until we have confirmed your booking and taken the payment. We will send all correspondence to the address provided when making the booking. For the avoidance of doubt, nothing in these terms shall entitle any third party to any benefit or rights pursuant to the Contracts (Rights of Third Parties) Act 1999.</p>
+    <h1 class="text-md-left mb-5 px-2">Accuracy of Information</h1>
 
-    <h1>Accuracy of Information</h1>
+    <p
+      class="text-md-left mb-5 px-2"
+    >Although we strive to ensure the accuracy of the information on this website, neither we nor our affiliates, suppliers or agents can be held responsible by you for the accuracy of such information. It is solely your responsibility to evaluate the accuracy, completeness and usefulness of all information provided on this website.</p>
 
-    <p>Although we strive to ensure the accuracy of the information on this website, neither we nor our affiliates, suppliers or agents can be held responsible by you for the accuracy of such information. It is solely your responsibility to evaluate the accuracy, completeness and usefulness of all information provided on this website.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >The Rentalcars.com voucher / eVoucher must be presented to the car hire company when picking up your hire car. We cannot be held responsible for rentals rejected if a voucher / eVoucher is not presented.</p>
 
-    <p>The Rentalcars.com voucher / eVoucher must be presented to the car hire company when picking up your hire car. We cannot be held responsible for rentals rejected if a voucher / eVoucher is not presented.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >We cannot accept liability for any charges incurred as a result of documentation signed for and accepted at the car hire counter.</p>
 
-    <p>We cannot accept liability for any charges incurred as a result of documentation signed for and accepted at the car hire counter.</p>
-
-    <p>We will not be held responsible for anyone driving under the influence of alcohol/drugs, causing wilful damage, driving off-road or driving without due care and attention. You, will accept full responsibility in respect of such claims. This list is not exhaustive.</p>
+    <p
+      class="text-md-left mb-5 px-2"
+    >We will not be held responsible for anyone driving under the influence of alcohol/drugs, causing wilful damage, driving off-road or driving without due care and attention. You, will accept full responsibility in respect of such claims. This list is not exhaustive.</p>
   </div>
 </template>F
