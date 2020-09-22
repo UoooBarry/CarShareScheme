0240b8985b4bbb7e77f09588bc1434c605d3c629
UoooBarry <s3719368@student.rmit.edu.au>
2020-09-22 22:13:33 +0800

    payment validation

--- src/Backend/carshare/src/routes/orders.ts
+++ src/Backend/carshare/src/routes/orders.ts
@@ -74,10 +74,13 @@ router.post('/extend/:id', [ExtendRentValidator.validate, verifyToken], async (r
         });
     } else {
         try {
-            //Update to complete
-            await _Rent.update(parseInt(req.params.id), { status: RentStatus.Completed });
             if (!req.originalRent) throw new ItemNotFound('Rent not found');
             const feeToPay = (req.body.period * req.originalRent.car.price + req.originalRent.car.price * 0.1).toFixed(2);
+            //If pass payment validator
+            if (req.body.payment_total !== feeToPay) throw new IncorrectItem('Payment amount incorrect');
+            //Update to complete
+            await _Rent.update(parseInt(req.params.id), { status: RentStatus.Completed });
+            
             //Create bill
             const bill = await _Bill.create({
                 user_id: req.user.id,
