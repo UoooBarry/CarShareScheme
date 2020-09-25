bd82d2882e11981b58d9d428c78116f88ce6e012
noobfromvn99 <s3673712@student.rmit.edu.au>
2020-09-26 02:50:45 +1000

    improve find neareast location page

--- src/Frontend/carshare/src/components/locations/locationRow.vue
+++ src/Frontend/carshare/src/components/locations/locationRow.vue
@@ -1,20 +1,59 @@
 <!-- @Author Yongqian Huang, created at 10/09/2020-->
 <template>
-    <tr>
-        <td>{{location.name}}</td>
-        <td><a :href='linkToGoogleMap' title="Redirect to google map.">{{location.address}}</a></td>
-        <td>{{(location.distance)/1000}} Km</td>
-    </tr>
+  <div>
+    <div class="row" style="background: white; padding-top:10px;padding-bottom:20px">
+      <div class="col">
+        <img id="location-img" src="../../../public/img/location.jpg" />
+      </div>
+      <div class="col">
+        <div class="location-name">{{location.name}}</div>
+        <div class="location-address">
+          <a :href="linkToGoogleMap" title="Redirect to google map.">{{location.address}}</a>
+        </div>
+        <div class="text-muted">{{(location.distance)/1000}} Km far away</div>
+        <div class="text-muted">
+          <font-awesome-icon icon="phone-alt" />
+          <u>(03) 9466 2866</u>
+        </div>
+      </div>
+    </div>
+    <div
+      class="row"
+      style="background: #dfdfdf; padding:10px; align-items: center;justify-content: center;"
+    >
+      <font-awesome-icon icon="clock" id="icon-clock" />Opening hours: 9:00 am - 5:00 pm
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'LocationRow',
-    props: ['location'],
-    data(){
-        return{
-            linkToGoogleMap: `https://www.google.com/maps/place/${this.location.address}/`
-        }
-    }
-}
+  name: "LocationRow",
+  props: ["location"],
+  data() {
+    return {
+      linkToGoogleMap: `https://www.google.com/maps/place/${this.location.address}/`
+    };
+  }
+};
 </script>
+<style scoped>
+#location-img {
+  margin-top: 0 !important;
+  width: 100%;
+  float: left;
+}
+.location-name {
+  font-weight: bold;
+  font-size: 16px;
+  margin-bottom: 10px;
+}
+.location-address a {
+  font-size: 14px;
+  color: black;
+}
+#icon-clock {
+  font-size: 16px;
+  margin-right: 10px;
+}
+</style>
--- src/Frontend/carshare/src/views/Locations.vue
+++ src/Frontend/carshare/src/views/Locations.vue
@@ -16,7 +16,7 @@ export default {
     data(){
         return{
             locations: [],
-            address: ''
+            address: '',
         }
     },
     components:{
